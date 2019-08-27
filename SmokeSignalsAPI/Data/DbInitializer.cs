cf7fb8978d2b23b7a38401c065eae047d9507317
GausiVagos <simon.gauthier@condorcet.be>
2019-08-27 15:30:28 +0200

    Patch 1.01
    Removed the added Participation in the Api => DBInitializer

--- SmokeSignalsAPI/Data/DbInitializer.cs
+++ SmokeSignalsAPI/Data/DbInitializer.cs
@@ -10,12 +10,6 @@ namespace SmokeSignalsAPI.Data
         public static void Initialize(SmokeSignalsContext context)
         {
             context.Database.EnsureCreated();
-
-            Participation p = new Participation { ChatId = 1, UserId = 1 };
-
-            if (!context.Participations.Any())
-                context.Participations.Add(p);
-
             context.SaveChanges();
         }
     }
